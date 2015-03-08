1179904dfade545e7f0b3b50c6cb686877271fcb
hiteshtheone <hiteshtheone@gmail.com>
2015-03-08 21:26:17 +0530

    First check-in,objects and map created but not tree

---
+++ parsor/start.cpp
@@ -0,0 +1,9 @@
+//
+//  start.cpp
+//  parsor
+//
+//  Created by hambarkh on 03/03/15.
+//  Copyright (c) 2015 Akola. All rights reserved.
+//
+
+#include "start.h"
---
+++ parsor/start.h
@@ -0,0 +1,14 @@
+//
+//  start.h
+//  parsor
+//
+//  Created by hambarkh on 03/03/15.
+//  Copyright (c) 2015 Akola. All rights reserved.
+//
+
+#ifndef __parsor__start__
+#define __parsor__start__
+
+#include <stdio.h>
+
+#endif /* defined(__parsor__start__) */
